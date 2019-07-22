d098573ef0817da10ac98531fa257bd86b7f6805
stoklosa.szymon <stoklosa.szymon@gmail.com>
2019-07-22 17:27:51 +0200

    Add project files.

---
+++ ASP_CORE_REACT/ClientApp/src/components/Users.js
@@ -0,0 +1,51 @@
+﻿import React, { Component } from 'react';
+
+export class Users extends Component {
+    static displayName = Users.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { forecasts: [], loading: true };
+
+        fetch('api/Users/GetUsers')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ forecasts: data, loading: false });
+            });
+    }
+
+    static renderForecastsTable(forecasts) {
+        return (
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Surname</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forecasts.map(forecast =>
+                        <tr key={forecast.name}>
+                            <td>{forecast.name}</td>
+                            <td>{forecast.surname}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Users.renderForecastsTable(this.state.forecasts);
+
+        return (
+            <div>
+                <h1>Weather forecast</h1>
+                <p>This component demonstrates fetching data from the server.</p>
+                {contents}
+            </div>
+        );
+    }
+}
---
+++ ASP_CORE_REACT/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_CORE_REACT.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("[action]")]
+        public IEnumerable<User> GetUsers()
+        {
+            return Enumerable.Range(1, 5).Select(index => new User
+            {
+                Name = "Jan",
+                Surname = "Nowak"
+            });
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
