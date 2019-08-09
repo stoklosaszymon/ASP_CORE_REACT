446fad01cf138a39924395c0202cfc369d782861
stoklosa.szymon <stoklosa.szymon@gmail.com>
2019-08-09 17:08:19 +0200

    removed components (fetchData, counter) and controller (sampleDataCOntroller) that were used to learn basics of react

--- ASP_CORE_REACT/ClientApp/src/App.js
+++ ASP_CORE_REACT/ClientApp/src/App.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Users } from './components/Users';
 import { Posts } from './components/Posts';
 import { Post } from './components/Post';
@@ -17,8 +15,6 @@ export default class App extends Component {
       return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/users' component={Users} />
         <Route path='/posts' component={Posts} />
         <Route path='/post/:id' component={Post} />
