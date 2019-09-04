f8437b3e1a6ea397742bc5c370012ad696c5b151
stoklosa.szymon <stoklosa.szymon@gmail.com>
2019-09-04 15:40:25 +0200

    separate chat input and chat messages. only logged users can chat

--- ASP_CORE_REACT/ClientApp/src/components/Chat.js
+++ ASP_CORE_REACT/ClientApp/src/components/Chat.js
@@ -1,25 +1,10 @@
 ﻿import React, { Component } from 'react';
 import * as signalR from '@aspnet/signalr';
+import { connect } from 'react-redux';
 
 export class Chat extends Component {
     constructor(props) {
         super(props);
-        console.log(signalR.VERSION);
-        this.hubConnection = new signalR.HubConnectionBuilder()
-            .withUrl("/chat")
-            .configureLogging(signalR.LogLevel.Information)
-            .build();
-
-        this.hubConnection.on('ReceiveMessage', (nick, receivedMessage) => {
-            const text = `${nick}: ${receivedMessage}`;
-            const messages = this.state.messages.concat([text]);
-            this.setState({ messages });
-        });
-
-        this.hubConnection
-            .start()
-            .then(() => console.log('Connection started!'))
-            .catch(err => console.log(err));
 
         this.state = {
             nick: '',
@@ -27,35 +12,77 @@ export class Chat extends Component {
             messages: [],
             hubConnection: null
         };
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    componentDidMount = () => {
+        const nick = this.props.logged && window.prompt('Your name:', this.props.loggedUserId.toString());
+
+        const hubConnection = new signalR.HubConnectionBuilder()
+            .withUrl("/chat")
+            .configureLogging(signalR.LogLevel.Information)
+            .build();
+
+        this.setState({ hubConnection, nick }, () => {
+            this.state.hubConnection
+                .start()
+                .then(() => console.log('Connection started!'))
+                .catch(err => console.log('Error while establishing connection :('));
+
+            this.state.hubConnection.on('ReceiveMessage', (nick, receivedMessage) => {
+                const text = `${nick}: ${receivedMessage}`;
+                const messages = this.state.messages.concat([text]);
+                this.setState({ messages });
+            });
+        });
     }
 
     sendMessage = () => {
-        this.hubConnection
+        this.state.hubConnection
             .invoke('SendMessage', this.state.nick, this.state.message)
             .catch(err => console.error(err));
 
         this.setState({ message: '' });
     }
 
+    onChange(event) {
+        this.setState({ message: event.target.value })
+    }
 
     render() {
         return (
-            <div>
-                <br />
-                <input
-                    type="text"
-                    value={this.state.message}
-                    onChange={e => this.setState({ message: e.target.value })}
-                />
-
-                <button onClick={this.sendMessage}>Send</button>
-
-                <div>
-                    {this.state.messages.map((message, index) => (
-                        <span style={{ display: 'block' }} key={index}> {message} </span>
-                    ))}
-                </div>
-            </div>
+        <div>
+            {
+                this.props.logged && <ChatInput sendMessage={this.sendMessage} onChange={this.onChange} message={this.state.message} />
+            }
+                <ChatPanel messages={this.state.messages}/>
+         </div>       
         );
     }
 }
+
+const ChatInput = ({ sendMessage, onChange, message }) => 
+    <div>
+        <br />
+        <input
+            type="text"
+            value={message}
+            onChange={onChange}
+        />
+
+        <button onClick={sendMessage}>Send</button>
+    </div>
+
+const ChatPanel = ({ messages }) => 
+    <div>
+        {messages.map((message, index) => (
+            <span style={{ display: 'block' }} key={index}> {message} </span>
+        ))}
+</div>
+
+const mapStateToProps = (state) => {
+    return { logged: state.logged, loggedUserId: state.loggedUserId };
+};
+
+export default Chat = connect(mapStateToProps)(Chat);
